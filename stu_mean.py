749f0bde11408c9f269e48017fb4ce9c8a4e4cc9
slau8 <slau8@stuy.edu>
2017-10-16 15:35:23 -0400

    did some :)

--- stu_mean.py
+++ stu_mean.py
@@ -1,30 +1,7 @@
 '''
-Shannon Lau
+Shannon Lau and Jennifer Zhang
 SoftDev1 pd 7
-HW09 -- No Treble
-2017-10-16
-'''
-
-import sqlite3   #enable control of an sqlite database
-import csv       #facilitates CSV I/O
-
-f = "discobandit.db"
-
-db = sqlite3.connect(f) #open if f exists, otherwise create
-c = db.cursor()    #facilitate db ops
-
-# Create table for 'peeps' data
-c.execute("CREATE TABLE peeps (name TEXT, age INTEGER, id INTEGER)")
-peeps = csv.DictReader(open("peeps.csv"))
-# Inserts data into 'peeps' table
-for row in peeps:
-    c.execute("INSERT INTO peeps VALUES ('" + row['name'] + "', " + row['age'] + ", " + row['id'] + ")")
-
-# Create table for 'courses' data
-'''
-Shannon Lau
-SoftDev1 pd 7
-HW09 -- No Treble
+HW10 -- Average
 2017-10-16
 '''
 
@@ -54,61 +31,16 @@ foo = c.execute("SELECT name,peeps.id,mark FROM peeps,courses WHERE peeps.id = c
 
 print foo
 
+c.execute("CREATE TABLE averages (name TEXT, id INTEGER, average INTEGER)")
+temp = 0 # previous id
+count = 0 # counts number of classes per student
+total = 0 # sum of student's marks
 for bar in foo:
+    if temp == bar[1]:
+        
     
-    
-db.commit() #save changes
-db.close()  #close database
-c.execute("CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER)")
-courses = csv.DictReader(open("courses.csv"))
-# Inserts data into 'course' table
-for row in courses:
-    c.execute("INSERT INTO courses VALUES ('" + row['code'] + "', " + row['mark'] + ", " + row['id'] + ")")
-
-foo = c.execute("SELECT name,peeps.id,mark FROM peeps,courses WHERE peeps.id = courses.id")
-
-for bar in foo:
-    print bar[0]
-
-'''
-Shannon Lau
-SoftDev1 pd 7
-HW09 -- No Treble
-2017-10-16
-'''
-
-import sqlite3   #enable control of an sqlite database
-import csv       #facilitates CSV I/O
-
-f = "discobandit.db"
-
-db = sqlite3.connect(f) #open if f exists, otherwise create
-c = db.cursor()    #facilitate db ops
-
-# Create table for 'peeps' data
-c.execute("CREATE TABLE peeps (name TEXT, age INTEGER, id INTEGER)")
-peeps = csv.DictReader(open("peeps.csv"))
-# Inserts data into 'peeps' table
-for row in peeps:
-    c.execute("INSERT INTO peeps VALUES ('" + row['name'] + "', " + row['age'] + ", " + row['id'] + ")")
-
-# Create table for 'courses' data
-c.execute("CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER)")
-courses = csv.DictReader(open("courses.csv"))
-# Inserts data into 'course' table
-for row in courses:
-    c.execute("INSERT INTO courses VALUES ('" + row['code'] + "', " + row['mark'] + ", " + row['id'] + ")")
-
-foo = c.execute("SELECT name,peeps.id,mark FROM peeps,courses WHERE peeps.id = courses.id")
-
-print foo
-
-for bar in foo:
     
     
 db.commit() #save changes
 db.close()  #close database
 
-
-db.commit() #save changes
-db.close()  #close database
