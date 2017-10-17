5a6b6ae68b1474d64d7a68093f0181a106b5f3d5
Jennifer Zhang <jennifer.newyork@aol.com>
2017-10-16 20:16:21 -0400

    worked on averages and deleted the database

--- stu_mean.py
+++ stu_mean.py
@@ -27,20 +27,39 @@ courses = csv.DictReader(open("courses.csv"))
 for row in courses:
     c.execute("INSERT INTO courses VALUES ('" + row['code'] + "', " + row['mark'] + ", " + row['id'] + ")")
 
-foo = c.execute("SELECT name,peeps.id,mark FROM peeps,courses WHERE peeps.id = courses.id")
+students = c.execute("SELECT name,peeps.id,mark FROM peeps,courses WHERE peeps.id = courses.id")
+# for student in students:
+#     print(student)
 
-print foo
-
-c.execute("CREATE TABLE averages (name TEXT, id INTEGER, average INTEGER)")
-temp = 0 # previous id
+#c.execute("CREATE TABLE averages (name TEXT, id INTEGER, average INTEGER)")
+first_id = True #checks to see if its the first occurence of that id
+curr_id = 0 # current id
 count = 0 # counts number of classes per student
 total = 0 # sum of student's marks
-for bar in foo:
-    if temp == bar[1]:
-        
-    
-    
-    
+
+averages = {'id': 'average'}
+for student in students:
+    #if student[1] == 1 or (student[1] == 2 and student[2] == 65):
+        #print(student)
+        if first_id:
+            curr_id = student[1]
+            first_id = False
+            #print('ran first_id')
+        if curr_id == student[1]:
+            total += student[2]
+            count+=1
+            #print('ran curr_id')
+        else:
+            averages[curr_id] = total / count
+            #print(total)
+            #print(count)
+
+            count = 0
+            total = 0
+            first_id = True
+            #print('ran else')
+
+print (averages)
+
 db.commit() #save changes
 db.close()  #close database
-
